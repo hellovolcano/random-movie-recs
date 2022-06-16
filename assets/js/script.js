cd34ef8f60d3d48fd62d21e6613a87139e3b338c
Valerie Gleason <valerie.gleason@gmail.com>
2022-06-16 11:10:12 -0500

    Adding OMDb search query and displaying movie info in a card in the main section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
+// element selectors
 var saveSettingsBtnEl = document.querySelector("#save-settings")
+var searchBtn = document.querySelector("#search-Btn")
+var cagedEl = document.querySelector("#caged")
+var posterEl = document.querySelector("#movie-poster")
+var movieInfoEl = document.querySelector("#movie-info")
+var movieCardEl = document.querySelector("#movie-card-div")
+var movieCardTitleEl = document.querySelector("#movie-title")
+var movieCardPlotEl = document.querySelector("#movie-plot")
+var streamingLinksEl = document.querySelector("#streaming-links")
+var searchTerm = document.querySelector("#search-query")
+
+var apiKey = "9bad881e"
 
 // Initialize empty array to store streaming service settings
 var streamingSettings = []
@@ -35,6 +47,9 @@ var saveSettingsHandler = function(event) {
 // save the settings to local storage
 var saveSettings = function(array) {
     localStorage.setItem("user-settings", JSON.stringify(array));
+
+    // handle the search button status if they've saved items
+    checkSettings(array)
 }
 
 // load user settings from local storage
@@ -52,6 +67,69 @@ var loadSettings = function() {
     }
 }
 
+// disables the search button if there are no settings saved and enables it if there are
+var checkSettings = function(array) {
+    if (array.length == 0) {
+        searchBtn.classList.add("disabled")
+    } else {
+        searchBtn.classList.remove("disabled")
+    }
+}
+
+// Make an OMDb API call to get information about the movie searched for
+var queryMovie = function() {
+    var apiUrl = "http://www.omdbapi.com/?t=" + searchTerm.value + "&apiKey=" + apiKey
+
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function(response) {
+        // request was successful
+        if (response.ok) {
+           response.json().then(function(data) {
+                getMovieInfo(data)
+           })
+        } else {
+            alert("Movie can't be found")
+        }
+    })
+}
+
+var getMovieInfo = function(array) {
+
+    // reset the content to blank before each search so things don't get weird
+    posterEl.textContent = ""
+
+    // unhide the movide card since we're ready to displaty info!
+    movieCardEl.classList.remove("movie-card")
+
+    // store these values in separate variables so we can easily push to an object and save
+    var movieTitle = array.Title
+    console.log(movieTitle)
+    var moviePlot = array.Plot
+    console.log(moviePlot)
+
+    var movieID = array.imdbID
+    console.log(movieID)
+
+    var posterLinkSrc = array.Poster    
+    var posterImg = document.createElement("img")
+    posterImg.setAttribute("src",posterLinkSrc)
+
+    // append the poster image to the poster img div
+    posterEl.append(posterImg)
+
+    // Assign the values to the correct spot in the card
+    movieCardTitleEl.textContent = movieTitle
+    movieCardPlotEl.textContent = moviePlot
+}
+
+// Function calls on page load
 loadSettings()
+checkSettings(streamingSettings)
+
+
 // save the settings when a user clicks the Save Settings button
 saveSettingsBtnEl.addEventListener("click" , saveSettingsHandler)
+
+// submit a query when we click the search button
+searchBtn.addEventListener("click", queryMovie)
